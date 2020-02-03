493c8842c97320cf7ea7a088b272f566f1f9fa60
atandiedwin <developeredwin2016@gmail.com>
2020-02-03 13:40:50 +0300

    Diabetes tracker duplicate

--- app/src/main/java/com/ian/diabetestracker/Constants/Links.java
+++ app/src/main/java/com/ian/diabetestracker/Constants/Links.java
@@ -2,18 +2,20 @@ package com.ian.diabetestracker.Constants;
 
 public class Links {
 
-    public static final  String FETCH_SUGAR_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/fetch_sugar_details.php";
-    public static final  String FETCH_PRESSURE_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/fetch_pressure_details.php";
-    public static final  String FETCH_WEIGHT_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/fetch_weight_details.php";
-    public static final  String FETCH_MEDICATION_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/fetch_medication_details.php";
-    public static final  String FETCH_A1C_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/fetch_a1c_details.php";
+    static final String BASE_URL = "http://192.168.43.50/Diabetes%20Tracker/";
+
+    public static final  String FETCH_SUGAR_DETAILS = BASE_URL +"fetch_sugar_details.php";
+    public static final  String FETCH_PRESSURE_DETAILS =BASE_URL + "fetch_pressure_details.php";
+    public static final  String FETCH_WEIGHT_DETAILS =BASE_URL+"fetch_weight_details.php";
+    public static final  String FETCH_MEDICATION_DETAILS =BASE_URL + "fetch_medication_details.php";
+    public static final  String FETCH_A1C_DETAILS =BASE_URL + "fetch_a1c_details.php";
 
 
-    public static final  String SAVE_A1C_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/save_A1c_details.php";
-    public static final  String SAVE_MEDICATION_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/save_medication_details.php";
-    public static final  String SAVE_WEIGHT_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/save_weight_details.php";
-    public static final  String SAVE_PRESSURE_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/save_pressure_details.php";
-    public static final  String SAVE_SUGAR_DETAILS ="http://192.168.137.244/Diabetes%20Tracker/save_sugar_details.php";
+    public static final  String SAVE_A1C_DETAILS =BASE_URL + "save_A1c_details.php";
+    public static final  String SAVE_MEDICATION_DETAILS =BASE_URL + "save_medication_details.php";
+    public static final  String SAVE_WEIGHT_DETAILS =BASE_URL + "save_weight_details.php";
+    public static final  String SAVE_PRESSURE_DETAILS =BASE_URL + "save_pressure_details.php";
+    public static final  String SAVE_SUGAR_DETAILS =BASE_URL + "save_sugar_details.php";
 
 
 }
